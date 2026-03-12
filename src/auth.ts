1c22a9f6c1c8c0091ec1e89fcf5b32afd3e02770
Subrata Mondal <subratamondal@tutanota.com>
2026-03-12 20:05:37 +0530

    chore: implement authentication handlers and proxy for NextAuth with Google provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "@/lib/prisma";
+import Google from "next-auth/providers/google";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Google],
+  // TODO: custom auth pages later
 });
