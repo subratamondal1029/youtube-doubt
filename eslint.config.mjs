8a97402f92781dc190087eafcc71d08e3b549392
Subrata Mondal <subratamondal@tutanota.com>
2026-03-12 13:21:36 +0530

    chore: implement husky pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,19 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
