6d1a8ffaf317c0e9e7f26e5483c68fbaded415d9
Subrata Mondal <subratamondal@tutanota.com>
2026-03-13 08:51:05 +0530

    refactor: simplify theme toggle component by removing unused imports and conditional rendering

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import * as React from "react";
-import { Moon, Sun, LaptopMinimal } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -13,19 +12,14 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme();
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          {theme === "light" && (
-            <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all  dark:-rotate-90" />
-          )}
-          {theme === "dark" && <Moon className="absolute h-[1.2rem] w-[1.2rem] transition-all" />}
-          {theme === "system" && (
-            <LaptopMinimal className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
-          )}
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
