b5ecaa9cfc9e7f29f3ced4465e8aa57d53e8da3b
Subrata Mondal <subratamondal@tutanota.com>
2026-03-16 18:45:44 +0530

    feat: add SideBarHeaderShortcut component with toggle and new chat functionality on collapsed

---
+++ src/components/layout/sidebar/sidebar-header-shortcut.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Logo from "@/components/Logo";
+import { useSidebar } from "@/components/ui/sidebar";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { CirclePlus, SidebarOpen } from "lucide-react";
+import Link from "next/link";
+
+const SideBarHeaderShortcut = () => {
+  const { state, setOpen } = useSidebar();
+
+  if (state === "expanded") return null;
+
+  return (
+    <div className="flex-col justify-between items-center gap-4 py-4 hidden group-data-[collapsible=icon]:flex border-b border-sidebar-border/70">
+      <div className="w-10 h-10 rounded-full border flex justify-center items-center">
+        <Logo href="/chat" />
+      </div>
+      <Tooltip>
+        <TooltipTrigger
+          onClick={() => setOpen(true)}
+          className="h-9 w-9 flex justify-center items-center hover:bg-white/10 rounded-lg"
+        >
+          <SidebarOpen className="h-4 w-4 cursor-pointer" />
+        </TooltipTrigger>
+        <TooltipContent side="right">Toggle Sidebar</TooltipContent>
+      </Tooltip>
+      <Tooltip>
+        <TooltipTrigger>
+          <Link
+            href="/chat"
+            className="flex h-9 w-9 items-center justify-center rounded-lg text-sidebar-foreground hover:bg-white/10 transition-colors"
+          >
+            <CirclePlus className="h-4 w-4" />
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent side="right">New Chat</TooltipContent>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default SideBarHeaderShortcut;
