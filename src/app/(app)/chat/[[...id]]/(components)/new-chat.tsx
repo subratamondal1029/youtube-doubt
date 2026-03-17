b187a9d0ae6b66cf837f463e8a3277dc6009726f
Subrata Mondal <subratamondal@tutanota.com>
2026-03-17 15:35:22 +0530

    feat: add NewChat component for initiating chats

---
+++ src/app/(app)/chat/[[...id]]/(components)/new-chat.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { Field, FieldDescription } from "@/components/ui/field";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import axios, { AxiosError } from "axios";
+import { Loader2, Send } from "lucide-react";
+import { redirect, RedirectType } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+function isValidUrl(url: string): boolean {
+  try {
+    if (!url) return true;
+    const u = new URL(url);
+    return u.protocol === "http:" || u.protocol === "https:";
+  } catch {
+    return false;
+  }
+}
+
+const NewChat = () => {
+  const [url, setUrl] = useState<string>("");
+  const [isValid, setIsValid] = useState<boolean>(true);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+  const submitHandler = async (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      setIsSubmitting(true);
+      const { data }: { data: { id: string } } = await axios.post("/api/chats", { url });
+
+      redirect(`/chat/${data.id}`, RedirectType.replace);
+    } catch (error) {
+      let errorMessage: string;
+      if (error instanceof AxiosError) {
+        errorMessage = error.response?.data?.message || "Failed to start chat";
+      } else {
+        errorMessage = "Failed to start chat";
+      }
+
+      toast.error(errorMessage, {
+        style: {
+          background: "red",
+          color: "white",
+        },
+        duration: 5000,
+        position: "bottom-left",
+      });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  useEffect(() => {
+    setIsValid(isValidUrl(url));
+  }, [url]);
+
+  return (
+    <>
+      <form className="w-full h-full flex justify-center items-center" onSubmit={submitHandler}>
+        <Field
+          orientation="horizontal"
+          className="w-full max-w-3/4 flex flex-col"
+          data-invalid={!isValid}
+        >
+          <InputGroup className="h-12 rounded-full px-1">
+            <InputGroupInput
+              placeholder="https://youtube.com/watch?v=ABCD"
+              className="text-base"
+              disabled={isSubmitting}
+              aria-invalid={!isValid}
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+            <InputGroupAddon align="inline-end">
+              <InputGroupButton
+                variant="secondary"
+                type="submit"
+                disabled={isSubmitting}
+                className="rounded-full cursor-pointer h-10 w-10 flex items-center justify-center"
+              >
+                {isSubmitting ? <Loader2 className="animate-spin" /> : <Send />}
+              </InputGroupButton>
+            </InputGroupAddon>
+          </InputGroup>
+          <FieldDescription className={`text-sm ${isValid && "invisible"}`}>
+            Please enter a valid URL
+          </FieldDescription>
+        </Field>
+      </form>
+    </>
+  );
+};
+
+export default NewChat;
--- src/app/(app)/chat/[[...id]]/page.tsx
+++ src/app/(app)/chat/[[...id]]/page.tsx
@@ -1,4 +1,16 @@
-import React from "react";
+import NewChat from "./(components)/new-chat";
+import Player from "./(components)/player";
+import Settings from "./(components)/settings";
+
+const getPage = (id?: string) => {
+  if (!id) return <NewChat />;
+
+  if (id === "settings") {
+    return <Settings />;
+  }
+
+  return <Player id={id} />;
+};
 
 type ChatProps = {
   params: Promise<{
@@ -9,7 +21,7 @@ type ChatProps = {
 const Chat = async ({ params }: ChatProps) => {
   const { id } = await params;
 
-  return <div>Chat {id}</div>;
+  return <div className="w-full min-h-screen pl-2">{getPage(id)}</div>;
 };
 
 export default Chat;
