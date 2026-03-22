97ce0a8f02cee0bf93073777190aba28560388dd
Subrata Mondal <subratamondal@tutanota.com>
2026-03-22 12:19:41 +0530

    feat: add AppContext and provider for managing application state

---
+++ src/app/context/AppContext.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { createContext, useContext, useState, ReactNode, SetStateAction, Dispatch } from "react";
+import type { History } from "@/components/layout/sidebar/chat-history";
+import { CHAT_LANGUAGE } from "../../../generated/prisma/enums";
+
+type AppState = {
+  timestamp: {
+    enable: boolean;
+    current: number;
+  };
+  chatLanguage: CHAT_LANGUAGE;
+  history: History[];
+
+  setTimestamp: Dispatch<SetStateAction<AppState["timestamp"]>>;
+  setChatLanguage: Dispatch<SetStateAction<AppState["chatLanguage"]>>;
+  setHistory: Dispatch<SetStateAction<AppState["history"]>>;
+};
+
+const AppContext = createContext<AppState | null>(null);
+
+const AppContextProvider = ({ children }: { children: ReactNode }) => {
+  const [timestamp, setTimestamp] = useState<AppState["timestamp"]>({
+    enable: true,
+    current: 0,
+  });
+
+  const [chatLanguage, setChatLanguage] = useState<AppState["chatLanguage"]>(CHAT_LANGUAGE.ENGLISH);
+
+  const [history, setHistory] = useState<AppState["history"]>([]);
+
+  const value = {
+    timestamp,
+    chatLanguage,
+    history,
+    setTimestamp,
+    setChatLanguage,
+    setHistory,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+const useAppContext = () => {
+  const ctx = useContext(AppContext);
+  if (!ctx) {
+    throw new Error("useAppContext must be used within an AppProvider");
+  }
+  return ctx;
+};
+
+export { AppContextProvider, useAppContext };
