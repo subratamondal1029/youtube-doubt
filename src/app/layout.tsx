67177e8561ede0073b60d6c21bf6e71d8443be64
Subrata Mondal <subratamondal@tutanota.com>
2026-03-22 23:31:49 +0530

    refactor: remove YouTube iframe API script from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout({
           <TooltipProvider>{children}</TooltipProvider>
           <Toaster />
         </ThemeProvider>
-        <script src="https://www.youtube.com/iframe_api" async></script>
       </body>
     </html>
   );
