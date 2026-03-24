ba738fdde76810cb05fbed1d9f299c1527249c04
Subrata Mondal <subratamondal@tutanota.com>
2026-03-24 19:24:49 +0530

    feat: implement getUserProfile function for user profile retrieval

---
+++ src/lib/repositories/user.repo.ts
@@ -0,0 +1,29 @@
+import { validate } from "uuid";
+import { ApiError } from "@/utils/ApiError";
+import prisma from "../prisma";
+
+const getUserProfile = async (userId: string) => {
+  try {
+    if (!userId || !validate(userId)) {
+      throw new ApiError(400, "Invalid user ID");
+    }
+
+    const profile = await prisma.profile.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (!profile) {
+      throw new ApiError(404, "Profile not found");
+    }
+
+    return profile;
+  } catch (error) {
+    if (error instanceof ApiError) {
+      throw error;
+    } else throw new ApiError(500, "An unexpected error occurred");
+  }
+};
+
+export { getUserProfile };
